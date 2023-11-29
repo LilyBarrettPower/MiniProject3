4ea454e35195c1e7a1a8380d5648ac705b5bef0f
Lily Barrett <11barrettpl@gmail.com>
2023-11-30 11:27:21 +1300

    done! just need to add comments now

--- controllers/populateDBcontroller.js
+++ controllers/populateDBcontroller.js
@@ -12,35 +12,75 @@ const populateDatabase = async (req, res) => {
         // await Flight.sync({ force: true }); // Force table creation on first run, remove in production
       
 
-        // Bulk create data for Rocket table
-        await Rocket.bulkCreate(spaceXData.map(launch => ({
-            rocketID: launch.rocket.rocket_id,
-            rocketType: launch.rocket.rocket_type,
-        })), { updateOnDuplicate: ['rocketID'] });
+        
+        await Promise.all(spaceXData.map(async (launch) => {
+            const [rocket, created] = await Rocket.findOrCreate({
+                where: { rocketID: launch.rocket.rocket_id },
+                defaults: {
+                    rocketType: launch.rocket.rocket_type,
+                },
+            });
+
+            console.log(`Rocket ${rocket.rocketID} ${created ? 'created' : 'already exists'}`);
+        }));
+
+        // await Rocket.bulkCreate(spaceXData.map(launch => ({
+        //     rocketID: launch.rocket.rocket_id,
+        //     rocketType: launch.rocket.rocket_type,
+        // })), { updateOnDuplicate: ['rocketID'] });
 
         console.log('Data inserted into Rocket table');
 
+
+        await Promise.all(spaceXData.map(async (launch) => {
+            const [site, created] = await LaunchSite.findOrCreate({
+                where: { siteID: launch.launch_site.site_id },
+                defaults: {
+                    siteName: launch.launch_site.site_name
+                },
+            });
+            console.log(`Launch Site ${site.siteID} ${created ? 'created' : 'already exists'}`);
+        }));
+
         // Bulk create data for LaunchSite table
-        await LaunchSite.bulkCreate(spaceXData.map(launch => ({
-            siteID: launch.launch_site.site_id,
-            siteName: launch.launch_site.site_name,
-        })), { updateOnDuplicate: ['siteID'] });
+        // await LaunchSite.bulkCreate(spaceXData.map(launch => ({
+        //     siteID: launch.launch_site.site_id,
+        //     siteName: launch.launch_site.site_name,
+        // })), { updateOnDuplicate: ['siteID'] });
 
         console.log('Data inserted into LaunchSite table');
 
+
+        await Promise.all(spaceXData.map(async (launch) => {
+            const [flight, created] = await Flight.findOrCreate({
+                where: { flightNumber: launch.flight_number },
+                defaults: {
+                    launchDate: launch.launch_date_local,
+                    payloadType: launch.payloads && Array.isArray(launch.payloads) ? launch.payloads[0].payload_type : null,
+                    launchSuccess: launch.launch_success,
+                    landSuccess: launch.land_success,
+                    videoLink: launch.links.video_link,
+                    rocketID: launch.rocket.rocket_id,
+                    rocketType: launch.rocket.rocket_type,
+                    siteID: launch.launch_site.site_id,
+                    siteName: launch.launch_site.site_name
+                },
+            });
+            console.log(`Flight ${flight.flightNumber} ${created ? 'created' : 'already exists'}`);
+        }));
         // Bulk create data for Flight table
-        await Flight.bulkCreate(spaceXData.map(launch => ({
-            flightNumber: launch.flight_number,
-            launchDate: launch.launch_date_local,
-            payloadType: launch.payloads && Array.isArray(launch.payloads) ? launch.payloads[0].payload_type : null,
-            launchSuccess: launch.launch_success,
-            landSuccess: launch.land_success,
-            videoLink: launch.links.video_link,
-            rocketID: launch.rocket.rocket_id,
-            rocketType: launch.rocket.rocket_type,
-            siteID: launch.launch_site.site_id,
-            siteName: launch.launch_site.site_name,
-        })), { updateOnDuplicate: ['launchDate'] });
+        // await Flight.bulkCreate(spaceXData.map(launch => ({
+        //     flightNumber: launch.flight_number,
+        //     launchDate: launch.launch_date_local,
+        //     payloadType: launch.payloads && Array.isArray(launch.payloads) ? launch.payloads[0].payload_type : null,
+        //     launchSuccess: launch.launch_success,
+        //     landSuccess: launch.land_success,
+        //     videoLink: launch.links.video_link,
+        //     rocketID: launch.rocket.rocket_id,
+        //     rocketType: launch.rocket.rocket_type,
+        //     siteID: launch.launch_site.site_id,
+        //     siteName: launch.launch_site.site_name,
+        // })), { updateOnDuplicate: ['launchDate'] });
 
         console.log('Data inserted into Flight table');
         res.status(200).json({ message: 'Data inserted into the database successfully' });
